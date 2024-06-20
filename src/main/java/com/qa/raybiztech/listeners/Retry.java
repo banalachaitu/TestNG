52736affa09c3bcde6d5286483da2361ebbfcc89
banalachaitu <72693724+banalachaitu@users.noreply.github.com>
2024-06-20 18:28:25 +1000

    Update Retry.java

--- src/main/java/com/qa/raybiztech/listeners/Retry.java
+++ src/main/java/com/qa/raybiztech/listeners/Retry.java
@@ -3,26 +3,24 @@ package com.qa.raybiztech.listeners;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-public class Retry  implements IRetryAnalyzer {
+public class Retry implements IRetryAnalyzer {
 
-	
-	
-	private int count = 0;
-	private static int maxTry = 3;
+    private int count = 0;
+    private static int maxTry = 3;
 
-	@Override
-	public boolean retry(ITestResult iTestResult) {
-		if (!iTestResult.isSuccess()) { // Check if test not succeed
-			if (count < maxTry) { // Check if max try count is reached
-				count++; // Increase the maxTry count by 1
-				iTestResult.setStatus(ITestResult.FAILURE); // Mark test as failed
-				return true; // Tells TestNG to re-run the test
-			} else {
-				iTestResult.setStatus(ITestResult.FAILURE); // If maxCount reached,test marked as failed
-			}
-		} else {
-			iTestResult.setStatus(ITestResult.SUCCESS); // If test passes, TestNG marks it as passed
-		}
-		return false;
-	}
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (!iTestResult.isSuccess()) {
+            if (count < maxTry) {
+                count++;
+                iTestResult.setStatus(ITestResult.FAILURE);
+                return true;
+            } else {
+                iTestResult.setStatus(ITestResult.FAILURE);
+            }
+        } else {
+            iTestResult.setStatus(ITestResult.SUCCESS);
+        }
+        return false;
+    }
 }
